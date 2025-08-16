156dab150233331316f6ceacb9b25ac4194b843f
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-08-15 21:15:55 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpotfyPlayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
